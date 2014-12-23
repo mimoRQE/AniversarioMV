56b2885a415e9860f0d0efb65214f0e0580664bf
mimoRQE <raul.quintero.e@gmail.com>
2014-12-23 09:57:03 -0600

    Return original code

--- app/src/main/java/com/quintero/raul/aniversariomv/fragment/OtherFragment.java
+++ app/src/main/java/com/quintero/raul/aniversariomv/fragment/OtherFragment.java
@@ -40,8 +40,6 @@ public class OtherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_other, container, false);
-        //regresa la vista
-        String NOMBRE;
         return rootView;
     }
 
