36d92e3038fcad0d8bc519671d8f6bddbeefa491
mimoRQE <raul.quintero.e@gmail.com>
2014-12-24 10:34:04 -0600

    Add changes in PhotoFragment landscape and protrait

--- app/src/main/java/com/quintero/raul/aniversariomv/fragment/PhotoFragment.java
+++ app/src/main/java/com/quintero/raul/aniversariomv/fragment/PhotoFragment.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.quintero.raul.aniversariomv.MainActivity;
@@ -46,12 +48,46 @@ public class PhotoFragment extends Fragment{
         TextView text2011= (TextView)rootView.findViewById(R.id.text_2011);
         TextView text2012= (TextView)rootView.findViewById(R.id.text_2012);
         TextView text2013= (TextView)rootView.findViewById(R.id.text_2013);
+        RelativeLayout layout2010 = (RelativeLayout)rootView.findViewById(R.id.layout_2010);
+        RelativeLayout layout2011 = (RelativeLayout)rootView.findViewById(R.id.layout_2011);
+        RelativeLayout layout2012 = (RelativeLayout)rootView.findViewById(R.id.layout_2012);
+        RelativeLayout layout2013 = (RelativeLayout)rootView.findViewById(R.id.layout_2013);
+        RelativeLayout layoutHover2010= (RelativeLayout)rootView.findViewById(R.id.image_hover_2010);
+        RelativeLayout layoutHover2011 = (RelativeLayout)rootView.findViewById(R.id.image_hover_2011);
+        RelativeLayout layoutHover2012 = (RelativeLayout)rootView.findViewById(R.id.image_hover_2012);
+        RelativeLayout layoutHover2013 = (RelativeLayout)rootView.findViewById(R.id.image_hover_2013);
+        setAnimaetedBacground(layout2010,layoutHover2010);
+        setAnimaetedBacground(layout2011,layoutHover2011);
+        setAnimaetedBacground(layout2012,layoutHover2012);
+        setAnimaetedBacground(layout2013,layoutHover2013);
         Font font = new Font();
         font.changeHame(getActivity(),text2010);
         font.changeHame(getActivity(),text2011);
         font.changeHame(getActivity(),text2012);
         font.changeHame(getActivity(),text2013);
         return rootView;
+    }
+
+
+    private void setAnimaetedBacground(RelativeLayout layout,final RelativeLayout layoutHover){
+
+        layout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        layoutHover.setVisibility(View.VISIBLE);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //set color back to default
+                        layoutHover.setVisibility(View.GONE);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
